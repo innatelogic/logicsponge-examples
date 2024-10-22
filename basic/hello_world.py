1626d030542a4026bd913f4951151501a23480a0
bollig <bollig@lmf.cnrs.fr>
2024-10-22 20:31:39 +0200

    added hello_world.py

---
+++ basic/hello_world.py
@@ -0,0 +1,28 @@
+import time
+
+import logicsponge.core as ls
+
+
+class Hello(ls.SourceTerm):
+    def run(self):
+        incomplete_message = "Hello"
+        out = ls.DataItem({"message": incomplete_message})
+        self.output(out)
+        time.sleep(1)
+
+
+class World(ls.FunctionTerm):
+    def f(self, item: ls.DataItem) -> ls.DataItem:
+        incomplete_message = item["message"]
+        complete_message = incomplete_message + " World!"
+        out = {"message": complete_message}
+        return ls.DataItem(out)
+
+
+sponge = (
+    Hello()
+    * World()
+    * ls.Print()
+)
+
+sponge.start()
